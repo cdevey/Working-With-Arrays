b07bf1ded23aa3804f66ee2b1d4e8e4c8129cc65
Cate Devey <cdevey@nd.edu>
2016-10-11 06:09:53 +0800

    should all be working now

--- run.rb
+++ run.rb
@@ -8,14 +8,16 @@ get '/' do
 end
 
 get '/years' do
-
-
+  @years=(1995..2016).to_a
 
   # code!
   erb :years, layout: :main
 end
 
 get '/states' do
+  @states=["New York","Maine","New Hampshire","Indiana","Vermont"]
+  @states.sort
+  @states.sort!
   # code!
   erb :states, layout: :main
 end
